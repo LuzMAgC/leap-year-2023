549dee046fa69b510d300bbca1611750429f6e28
katarn <katarn@katarn.es>
2023-11-24 19:26:51 +0100

    GREEN: test_1_is_not_leap

--- python/src/leap_year.py
+++ python/src/leap_year.py
@@ -2,6 +2,6 @@ class LeapYear:
     def is_leap_year(
         self,year:int
     ) -> bool:
-        if year == 3:
+        if year == 3 or year == 1:
             return False
         return True
