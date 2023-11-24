4021ddcdb82ab1a10722180506f5041c7bbdb661
IgnacioJimenezC <ijimenez@iesrafaelalberti.es>
2023-11-24 19:29:56 +0100

    GREEN: test_7_is_not_leap

--- python/src/leap_year.py
+++ python/src/leap_year.py
@@ -2,6 +2,6 @@ class LeapYear:
     def is_leap_year(
         self,year:int
     ) -> bool:
-        if year == 3 or year == 1:
+        if year == 3 or year == 1 or year == 7:
             return False
         return True
