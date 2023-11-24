2b5497726e58840590498442ec7390311ede8e67
katarn <katarn@katarn.es>
2023-11-24 19:34:16 +0100

    GREEN: test_1800_is_not_leap

--- python/src/leap_year.py
+++ python/src/leap_year.py
@@ -2,6 +2,8 @@ class LeapYear:
     def is_leap_year(
         self,year:int
     ) -> bool:
+        if year == 1800:
+            return False
         if year % 4 != 0:
             return False
         return True
